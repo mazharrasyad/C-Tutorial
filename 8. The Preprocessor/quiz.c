e40ef0667f6e494a4bf409cc7fe4f1d7e9a838ac
mazharrasyad <muhazharrasyad@gmail.com>
2019-01-16 13:55:40 +0700

    Edit

--- 8. The Preprocessor/quiz.c
+++ 8. The Preprocessor/quiz.c
@@ -1,12 +1,10 @@
 // No 1
-
 #define TRIPLE(x) (x) * 3
 
 int num = 42;
 printf("%d",TRIPLE(num));
 
 // No 2
-
 #ifdef TRIPLE
 #define SQR(x) (x) * (x)
 #else
@@ -14,11 +12,9 @@ printf("%d",TRIPLE(num));
 #endif
 
 // No 3
-
 // Thepreprocessor acts: before compilation
 
 // No 4
-
 #include <stdio.h>
 #define T 42
 int main()
@@ -29,7 +25,6 @@ int main()
 }
 
 // No 5
-
 #define sqr(x) x * x
 int x = 16 / sqr(4);
 printf("%d",x);
